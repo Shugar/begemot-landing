3bfaed0abfaa64780a3ff8128870e2063bf42529
Тигран Тумасов <shugar348@tumasov.local>
2015-03-20 19:31:14 +0300

    make odometer and start responsive

---
+++ app/main.js
@@ -0,0 +1,7 @@
+setTimeout(function(){
+    $('.odometer.od1').html(12);
+    $('.odometer.od2').html(39);
+    $('.odometer.od3').html(4);
+    $('.odometer.od4').html(6000000);
+    $('.odometer.od5').html(10);
+  });
